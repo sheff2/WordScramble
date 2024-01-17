0999da7c2fbadb17a38afbcc16af8386f4f0e2fd
sheff2 <shaheff01@gmail.com>
2024-01-16 19:41:31 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Shaun Heffernan on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
